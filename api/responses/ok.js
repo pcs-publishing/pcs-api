9e262d7e6b8f9c0c34e5f617581daf36e7cd5f55
Michael Rogers <mrogers@pcs-publishing.com>
2018-08-21 17:19:15 +0100

    MR1314 - apply standard.js

--- api/responses/ok.js
+++ api/responses/ok.js
@@ -13,36 +13,34 @@
  *          - pass string to render specified view
  */
 
-module.exports = function sendOK(data, statusCode) {
-    const req = this.req,
-        res = this.res;
+module.exports = function sendOK (data, statusCode) {
+  const res = this.res
 
-    return respond(res, cleanResponseData(data), statusCode);
-};
+  return respond(res, cleanResponseData(data), statusCode)
+}
 
 /**
  * Remove toJSON functions left by model instances before passing on to res.json which calls JSON.stringify (which in turn calls toJSON at all levels)
  *
- * @param {Object} data The data to remove toJSON functions from 
+ * @param {Object} data The data to remove toJSON functions from
  *
  * @return {Object} data The data with toJSON functions removed
  */
-function cleanResponseData(data) {
-    for (prop in data) {
-        if (prop === 'toJSON') {
-            data[prop] = undefined;
-        } else if (_.isObject(data[prop]))
-            cleanResponseData(data[prop]);
-    }
+function cleanResponseData (data) {
+  for (let prop in data) {
+    if (prop === 'toJSON') {
+      data[prop] = undefined
+    } else if (_.isObject(data[prop])) { cleanResponseData(data[prop]) }
+  }
 
-    return data;
+  return data
 }
 
-function respond(res, record) {
-    pcsapi.log.verbose(`ok.js - respond - Sending 200 response to ${res.req.originalUrl} request`);
+function respond (res, record) {
+  pcsapi.log.verbose(`ok.js - respond - Sending 200 response to ${res.req.originalUrl} request`)
 
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify(record));
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  })
+  res.end(JSON.stringify(record))
 }
--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-'use strict';
+'use strict'
 
-const config = require(`${process.cwd()}/config/local.js`),
-    path = require('path'),
-    httpServer = require('./lib/http-server'),
-    logger = require('./lib/logger'),
-    _ = require('@sailshq/lodash'),
-    includeAll = require('include-all'),
-    connections = require('./lib/connections'),
-    bluebird = require('bluebird'),
-    moduleLoader = require('./lib/module-loader');
+const config = require(`${process.cwd()}/config/local.js`)
+const httpServer = require('./lib/http-server')
+const logger = require('./lib/logger')
+const _ = require('@sailshq/lodash')
+const connections = require('./lib/connections')
+const bluebird = require('bluebird')
+const moduleLoader = require('./lib/module-loader')
 
 module.exports = {
 
-    initialise: function() {
+  initialise: function () {
+    console.log(config)
 
-        console.log(config);
+    global['_'] = _
 
-        global['_'] = _;
-
-        global['pcsapi'] = {
-            Promise: bluebird,
-            services: {},
-            middleware: {
-                controllers: {},
-                models: {},
-                responses: {},
-                mixins: {}
-            },
-            connections: {},
-            moduleLoader: moduleLoader
-        };
-
-        pcsapi.log = logger;
-
-        pcsapi.log.debug(`index.js - initialise - Initialised`);
-
-        connections.create();
-    },
-
-    load: function() {
-        pcsapi.log.debug(`index.js - load - Loading`);
-
-        moduleLoader.load();
-        httpServer.createServer();
+    global['pcsapi'] = {
+      Promise: bluebird,
+      services: {},
+      middleware: {
+        controllers: {},
+        models: {},
+        responses: {},
+        mixins: {}
+      },
+      connections: {},
+      moduleLoader: moduleLoader
     }
-};
+
+    pcsapi.log = logger
+
+    pcsapi.log.debug(`index.js - initialise - Initialised`)
+
+    connections.create()
+  },
+
+  load: function () {
+    pcsapi.log.debug(`index.js - load - Loading`)
+
+    moduleLoader.load()
+    httpServer.createServer()
+  }
+}
